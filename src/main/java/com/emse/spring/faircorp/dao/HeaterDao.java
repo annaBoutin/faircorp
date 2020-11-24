360fb8e111edda028e6927b3679ee7459ce143fe
Anna Boutin <anna.boutin@etu.emse.fr>
2020-11-24 14:04:09 +0100

    Fin Lab jpa

--- src/main/java/com/emse/spring/faircorp/dao/HeaterDao.java
+++ src/main/java/com/emse/spring/faircorp/dao/HeaterDao.java
@@ -2,6 +2,12 @@ package com.emse.spring.faircorp.dao;
 
 import com.emse.spring.faircorp.model.Heater;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface HeaterDao  extends JpaRepository<Heater, Long>, HeaterCustomDao {
+    @Modifying
+    @Query("delete from Heater h where h.room.id = :roomId")
+    void deleteByRoomId(@Param("roomId")Long roomId);
 }
--- src/test/java/com/emse/spring/faircorp/dao/HeaterDaoTest.java
+++ src/test/java/com/emse/spring/faircorp/dao/HeaterDaoTest.java
@@ -2,6 +2,8 @@ package com.emse.spring.faircorp.dao;
 
 import com.emse.spring.faircorp.model.Heater;
 import com.emse.spring.faircorp.model.HeaterStatus;
+import com.emse.spring.faircorp.model.Room;
+import com.emse.spring.faircorp.model.Window;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,16 +11,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
 public class HeaterDaoTest {
     @Autowired
     private HeaterDao heaterDao;
 
+    @Autowired
+    private RoomDao roomDao;
+
     @Test
     public void shouldFindAHeater() {
         Heater heater = heaterDao.getOne(-10L);
         Assertions.assertThat(heater.getName()).isEqualTo("Heater1");
         Assertions.assertThat(heater.getHeaterStatus()).isEqualTo(HeaterStatus.ON);
+    }
+
+    @Test
+    public void shouldDeleteWindowsRoom() {
+        Optional<Room> room = roomDao.findById(-10L);
+        Assertions.assertThat(room.get().getHeaters().size()).isEqualTo(2);
+        heaterDao.deleteByRoomId(-10L);
+        List<Heater> result = heaterDao.findAllById(room.get().getWindows().stream().map(Window::getId).collect(Collectors.toList()));
+        Assertions.assertThat(result).isEmpty();
     }
 }
