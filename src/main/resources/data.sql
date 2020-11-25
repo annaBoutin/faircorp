80507fa3e2b6207a1c5380a81cdd561b78452fc5
Anna Boutin <anna.boutin@etu.emse.fr>
2020-11-25 15:42:49 +0100

    API REST OK

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO ROOM(id, name, floor, current_temp, target_temp, building_id) VALUES(-10, 'Room1', 1, 22.3, 20.0,-2);
+INSERT INTO ROOM(id, name, floor, current_temp, target_temp) VALUES(-10, 'Room1', 1, 22.3, 20.0);
 INSERT INTO ROOM(id, name, floor) VALUES(-9, 'Room2', 1);
 
 INSERT INTO HEATER(id, heater_status, name, power, room_id) VALUES(-10, 'ON', 'Heater1', 2000, -10);
@@ -9,4 +9,3 @@ INSERT INTO RWINDOW(id, window_status, name, room_id) VALUES(-9, 'CLOSED', 'Wind
 INSERT INTO RWINDOW(id, window_status, name, room_id) VALUES(-8, 'OPEN', 'Window 1', -9);
 INSERT INTO RWINDOW(id, window_status, name, room_id) VALUES(-7, 'CLOSED', 'Window 2', -9);
 
-INSERT INTO BUILDING(id) VALUES (-2);
